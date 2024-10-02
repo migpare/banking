e3f116b7ebe75968a37ac7bf326cd78271b0fe72
miguelpare <migparee@gmail.com>
2024-10-02 16:00:03 -0400

    main

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Miguel' };
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            account={1}
+            totalBanks={1}
+            totalCurrentBalance={1250.00}
+          />
+        </header>
+        </div>
+        </section>
   )
 }
 
